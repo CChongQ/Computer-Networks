4bd4530ad83f1258dd95a05e526983cbf6722ab1
Claire Qiu <claireqch@gmail.com>
2021-02-03 21:49:13 -0500

    lab1 - File transfer section 1 complete

--- Lab1/deliver.c
+++ Lab1/deliver.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <ctype.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -11,6 +12,7 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+
 int main(int argc, char const *argv[]){
     
     if (argc != 3) { 
@@ -18,32 +20,36 @@ int main(int argc, char const *argv[]){
         exit(1);
     }
 
-    //get host address +  port number
-    int host = argv[1];
+    //get host name +  port number
+    const char *hostname = argv[1];
     int port_num = atoi(argv[2]);
-
+    
     //create socket
     int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if(sockfd < 0) {
         printf("Error: Create socket failed\n");
         exit(1);
     }
-
     struct sockaddr_in server;  
     memset(&server, 0, sizeof server);
     server.sin_family = AF_INET;
-    int hp = gethostbyname(host);
-    if (hp==0){
-        printf("Error: Unknown host\n");
-        exit(1);
-    }
     server.sin_port= htons(port_num);
+    //get ip using host name
+    struct hostent *he = gethostbyname(hostname) ;
+	struct in_addr **addr_list;
+    if ( he == NULL) {
+		printf("Error: Unknown host\n");
+		exit(1);
+	}
+    addr_list = (struct in_addr **) he->h_addr_list;
+    server.sin_addr = *addr_list[0];
 
     //require user input
     int max_input= 1024;
     char userInput[max_input];
-    printf("Please input input name using this format: ftp <file name>\n");
+    printf("Please input file name using this format: ftp <file name>\n");
     fgets(userInput,max_input, stdin);
+
     //get filename
     char *search = " ";
     char *token_1 = strtok(userInput, search);
@@ -52,13 +58,22 @@ int main(int argc, char const *argv[]){
         printf("Error: Input must begin with ftp\n");
         exit(1);
     }
-    else if (access(fileName, F_OK) != 0 ){
-        printf("Error: File < %c > does not exists\n",fileName);
+    if (*fileName == 0){
+         printf("Error: filename cannot be empty");
+    }
+    //trim filename
+    char *end;
+    end = fileName + strlen(fileName) - 1;
+    while(end > fileName && isspace((unsigned char)*end)) end--;
+    end[1] = '\0';
+
+    if (access(fileName, F_OK) != 0 ){
+        fprintf(stderr,"Error: File < %s > does not exists\n",fileName);
         exit(1);
     }
     else{
-        //send a message “ftp” to the server
-        int num_bytes = sendto(sockfd,"ftp",strlen("ftp"),0(struct sockaddr *)&server,sizeof(server));
+        //send a message Â“ftpÂ” to the server
+        int num_bytes = sendto(sockfd,"ftp",strlen("ftp"),0,(struct sockaddr *)&server,sizeof(server));
         if (num_bytes<0){
             printf("Error: Send message 'ftp' to the server failed.\n");
             exit(1);
@@ -67,14 +82,15 @@ int main(int argc, char const *argv[]){
         struct sockaddr_in from;  
         int buf_size = 1024;
         char buf[buf_size];
-        int num_bytes_from = recvfrom(sockfd,buf,buf_size,0,(struct sockaddr *)&from,sizeof(from));
+        socklen_t from_len = sizeof(from);
+        int num_bytes_from = recvfrom(sockfd,buf,buf_size,0,(struct sockaddr *)&from,&from_len);
         if (num_bytes_from<0){
             printf("Error: recvfrom\n");
             exit(1);
         }
 
         if (strcmp(buf,"yes") == 0){
-             printf("Error: A file transfer can start.\n");
+             printf("A file transfer can start.\n");
         }
         else{
             printf("Error: Received message is not 'yes'\n");
--- Lab1/server.c
+++ Lab1/server.c
@@ -20,13 +20,12 @@ int main(int argc, char const *argv[]){
     //get port number
     int port_num = atoi(argv[1]);
 
-    //create socket
+    //create sockets
     int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if(sockfd < 0) {
         printf("Error: Create socket failed\n");
         exit(1);
     }
-
     struct sockaddr_in server;  
     server.sin_family = AF_INET;
     server.sin_port= htons(port_num);
@@ -43,7 +42,9 @@ int main(int argc, char const *argv[]){
     struct sockaddr_in from;  
     int buf_size = 1024;
     char buf[buf_size];
-    int num_bytes = recvfrom(sockfd,buf,buf_size,0,(struct sockaddr *)&from,sizeof(from));
+    memset(buf, '\0', buf_size);
+    socklen_t from_len = sizeof(from);
+    int num_bytes = recvfrom(sockfd,buf,buf_size,0,(struct sockaddr *)&from,&from_len);
     if (num_bytes<0){
         printf("Error: recvfrom\n");
         exit(1);
@@ -51,7 +52,7 @@ int main(int argc, char const *argv[]){
 
     //send message to client
     int num_bytes_sent;
-    if ( stecmp(buf,"ftp") == 0 ){
+    if ( strcmp(buf,"ftp") == 0 ){
         char msg[] = "yes";
         num_bytes_sent = sendto(sockfd,msg,strlen(msg),0,(struct sockaddr *)&from,sizeof(from));
     }
@@ -59,7 +60,7 @@ int main(int argc, char const *argv[]){
         char msg[] = "no";
         num_bytes_sent = sendto(sockfd,msg,strlen(msg),0,(struct sockaddr *)&from,sizeof(from));
     }
-
+ 
     if (num_bytes_sent<0){
         printf("Error: Reply message  to the client failed.\n");
         exit(1);
@@ -67,6 +68,5 @@ int main(int argc, char const *argv[]){
 
     close(sockfd);
     return (0);
-
 
 }
