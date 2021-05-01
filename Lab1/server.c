65564ea9ac12574685255ae691fbabada6be743a
Claire Qiu <claireqch@gmail.com>
2021-05-01 14:18:09 -0400

    Lab1 code - update

--- Lab1/server.c
+++ Lab1/server.c
@@ -11,6 +11,15 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+// #define MAX_LEN 1000
+// #define ACK "ACK"
+// #define NACK "NACK"
+// #define BUF_LEN 1024
+
+#include "packet.h"
+
+struct packet *  detach_packet(char* buf,struct packet* return_packet);
+
 int main(int argc, char const *argv[]){
 
     if (argc != 2) { 
@@ -40,11 +49,11 @@ int main(int argc, char const *argv[]){
 
     //listen: receive connection request 
     struct sockaddr_in from;  
-    int buf_size = 1024;
-    char buf[buf_size];
-    memset(buf, '\0', buf_size);
+    // int buf_size = 1024;
+    char buf[BUF_LEN];
+    memset(buf, '\0', BUF_LEN);
     socklen_t from_len = sizeof(from);
-    int num_bytes = recvfrom(sockfd,buf,buf_size,0,(struct sockaddr *)&from,&from_len);
+    int num_bytes = recvfrom(sockfd,buf,BUF_LEN,0,(struct sockaddr *)&from,&from_len);
     if (num_bytes<0){
         printf("Error: recvfrom\n");
         exit(1);
@@ -52,7 +61,7 @@ int main(int argc, char const *argv[]){
 
     //send message to client
     int num_bytes_sent;
-    if ( strcmp(buf,"ftp") == 0 ){
+    if ( strcmp(buf,"ftp") == 0 ){ 
         char msg[] = "yes";
         num_bytes_sent = sendto(sockfd,msg,strlen(msg),0,(struct sockaddr *)&from,sizeof(from));
     }
@@ -60,13 +69,104 @@ int main(int argc, char const *argv[]){
         char msg[] = "no";
         num_bytes_sent = sendto(sockfd,msg,strlen(msg),0,(struct sockaddr *)&from,sizeof(from));
     }
- 
+    printf("HERE!\n");
     if (num_bytes_sent<0){
         printf("Error: Reply message  to the client failed.\n");
         exit(1);
     }
+    else{
+        struct sockaddr_in from;  
+        socklen_t from_len = sizeof(from);
+    
+        FILE * file_tb_write;
 
+        while(1){
+            //recevie
+            char buf[BUF_LEN];
+            memset(buf, '\0', BUF_LEN);
+            printf("HERE 1!\n");
+            int num_bytes = recvfrom(sockfd,buf,BUF_LEN,0,(struct sockaddr *)&from,&from_len);
+            // printf("Server buf %s fragment\n",buf);
+            printf("HERE 2!\n");
+            if (num_bytes<0){
+                printf("Server Error: recvfrom\n");
+                char msg[] = NACK;
+                num_bytes_sent = sendto(sockfd,msg,strlen(msg),0,(struct sockaddr *)&from,sizeof(from));
+                if (num_bytes_sent<0){
+                    printf("Error: Reply message to the client failed.\n");
+                    exit(1);
+                }
+            }
+            else{
+                //detach data
+                printf("HERE 3!\n");
+                struct packet temp_packet;
+                detach_packet(buf,&temp_packet);
+                printf("Processing the %d fragment\n",temp_packet.frag_no );
+                printf("HERE 4!\n");
+                if (temp_packet.frag_no == 1){//first fragment
+
+                    char c[] = "recv_";
+                    strcat(c,temp_packet.filename);
+                    printf(" Server side file name is %s \n", c );
+
+                    file_tb_write = fopen(c, "wb");
+                }
+
+                fwrite(temp_packet.filedata, sizeof(char), temp_packet.size, file_tb_write);
+
+
+                //send ack or nack
+                char msg[] = ACK;
+                num_bytes_sent = sendto(sockfd,msg,strlen(msg),0,(struct sockaddr *)&from,sizeof(from));
+                if (num_bytes_sent<0){
+                    printf("Error: Reply message to the client failed.\n");
+                    exit(1);
+                }
+                if (temp_packet.frag_no == temp_packet.total_frag){
+                    printf("Processed ALL fragment\n");
+                    break;
+                }
+                
+            }
+        }
+        fclose(file_tb_write);
+       
+    }
+
+    // freeaddrinfo(server);
     close(sockfd);
     return (0);
+
+}
+
+struct packet *  detach_packet(char* buf,struct packet* return_packet){
+
+    unsigned int total_frag,frag_no,size;
+    char* filename;
+
+    total_frag = atoi(strtok(buf, ":"));
+    printf("Server total_frag %d \n",total_frag);
+    frag_no = atoi(strtok(NULL, ":"));
+    printf("Server frag_no %d \n",frag_no);
+    size = atoi(strtok(NULL, ":"));
+    printf("Server size %d \n",size);
+    filename = strtok(NULL, ":");
+    printf("Server filename %s \n",filename);
+
+    char * fileData = strtok(NULL, "");
+    printf("Server fileData %s \n",fileData);
+
+    // struct packet* return_packet;
+    // return_packet = malloc(size*sizeof(char));
+    return_packet->total_frag = total_frag;
+    return_packet->frag_no = frag_no;
+    return_packet->size = size;
+    return_packet->filename = filename;
+    memcpy(return_packet->filedata, fileData, return_packet->size );
+    printf("End\n");
+
+    return return_packet;
+
 
 }
